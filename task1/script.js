ebaf7ebfcf3d21bea304b0e7ae1bc05379a4ced3
Regina Putri <reginaputria2003@gmail.com>
2020-07-17 10:06:12 +0700

    first commit

---
+++ task1/script.js
@@ -0,0 +1,33 @@
+const biodata = {
+    name: "Regina Putri",
+    age: 17,
+    hobbies: ['mendengarkan musik', 'main hp'],
+    isMarried: false,
+    schoolList: [
+        {
+            name: "SMPN 3 Purwokerto",
+            yearIn: 2015,
+            yearOut: 2017,
+            major: null
+        },
+        {
+            name: "SMK Telkom Purwokerto",
+            yearIn: 2017,
+            yearOut: 2020,
+            major: null
+        },
+    ],
+    skills: [
+        {
+            skillName: "Web Development",
+            level: "advanced"
+        },
+        {
+            skillName: "React Native",
+            level: "advanced"
+        }
+    ],
+    interestInCoding: true
+};
+
+console.log(biodata);
---
+++ task3/script.js
@@ -0,0 +1,8 @@
+const printSegitiga = 5
+
+for(let i=printSegitiga; i>=1; i--){
+    for(let j=1; j<=i; j++){
+        console.log(j+ "");
+    }
+    console.log();
+}
