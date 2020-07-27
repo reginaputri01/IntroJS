6fb95e13f8b468623f5e9ec744b91bb0675c7d37
Regina Putri <reginaputria2003@gmail.com>
2020-07-27 17:08:56 +0700

    perbaikan

--- week2/CodeImplementation/DivideAndSort.js
+++ week2/CodeImplementation/DivideAndSort.js
@@ -1,6 +1,11 @@
 const divideAndSort = (number) => {
-  const result = number.split('0')
-  console.log(result.join(""));
+  const divide = number.toString().split('0')
+  let result = ""
+  for(let i = 0; i < divide.length; i++){
+    let output = divide[i].split('').sort().join('')
+    result += output
+  }
+  console.log(result)
 }
 
-divideAndSort("43240152340232409786")
+divideAndSort(5956560159466056)
